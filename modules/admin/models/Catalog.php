84601dd89378c6f685bb5f2972dc6113b2562eb2
aleonov <aleonov@tepsvet.ru>
2017-10-18 15:10:35 +0300

    Новые элементы каталога

--- modules/admin/models/Catalog.php
+++ modules/admin/models/Catalog.php
@@ -2,19 +2,9 @@
 
 namespace app\modules\admin\models;
 
-use Yii;
+use \yii\db\ActiveRecord;
 
-/**
- * This is the model class for table "{{%catalog}}".
- *
- * @property integer $id
- * @property integer $status
- * @property string $name
- * @property string $img
- * @property double $file_size
- * @property string $file_puth_pdf
- */
-class Catalog extends \yii\db\ActiveRecord
+class Catalog extends ActiveRecord
 {
     /**
      * @inheritdoc
@@ -54,38 +44,9 @@ class Catalog extends \yii\db\ActiveRecord
 
     public function setFileSize($file)
     {
-        $fileMb = filesize($_SERVER['DOCUMENT_ROOT'] . $file) / (1024 * 1024);
+        $f = \Yii::getAlias('@web') . $file;
+        $fileMb = filesize($f) / (1024 * 1024);
         $this->file_size = round($fileMb);
     }
-
-//    public function getFileSize($file)
-//    {
-//        $fileSize = filesize($file);
-//        // Если размер больше 1 Кб
-//        if ($fileSize > 1024) {
-//            $fileSize = ($fileSize / 1024);
-//            // Если размер файла больше Килобайта
-//            // то лучше отобразить его в Мегабайтах. Пересчитываем в Мб
-//            if ($fileSize > 1024) {
-//                $fileSize = ($fileSize / 1024);
-//                // А уж если файл больше 1 Мегабайта, то проверяем
-//                // Не больше ли он 1 Гигабайта
-//                if ($fileSize > 1024) {
-//                    $fileSize = ($fileSize / 1024);
-//                    $fileSize = round($fileSize, 1);
-//                    return $fileSize . " Гб";
-//                } else {
-//                    $fileSize = round($fileSize, 1);
-//                    return $fileSize . " Mб";
-//                }
-//            } else {
-//                $fileSize = round($fileSize, 1);
-//                return $fileSize . " Кб";
-//            }
-//        } else {
-//            $fileSize = round($fileSize, 1);
-//            return $fileSize . " байт";
-//        }
-//    }
-
 }
+for()
